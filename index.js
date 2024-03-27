5721616978a82d7595ae4eb7591373708943228b
Jan Muhachi <jan.muhachi@gmail.com>
2024-03-27 12:48:44 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,23 @@
+// arrow functions 
+
+
+// write math arrow functions
+
+
+// divide
+let divide = (a, b) => a / b;
+// multiply
+let multiply = (x, y) => x * y;
+
+// add
+let add = (a, b) => a + b;
+
+//subtract
+let subtraction =(a, b) => a - b;
+
+
+
+
+
+
+//work written like this because i am not sure if this is what the instruction needed me to do.
